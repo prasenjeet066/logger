1fe2dff0bee639e3b1f26864104a65dde2f27a50
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-07-15 16:20:23 +0600

    Update connection.ts

--- lib/mongodb/connection.ts
+++ lib/mongodb/connection.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = "mongodb+srv://howladerprasenjeet440:6ehMc6iIeh2SDCvJ@cluster0.joo9psn.mongodb.net/codes_beta"
 
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable inside .env.local")
