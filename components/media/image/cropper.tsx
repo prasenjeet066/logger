6fc22256784bc89698fa20539f63c1520b76c5a4
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-07-23 14:33:42 +0600

    update

---
+++ components/media/image/cropper.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Cropper, CropperRef } from 'react-advanced-cropper';
+
+export const Cropper = ({imageUrl}) => {
+    const cropperRef = useRef<CropperRef>(null);
+    const zoom = () => {
+        if (cropperRef.current) {
+            cropperRef.current.zoomImage(2); // zoom-in 2x
+        }
+    };
+    const move = () => {
+        if (cropperRef.current) {
+            cropperRef.current.moveImage(50, 100); // move x = 50, y = 100
+        }
+    };
+    return (
+        <Cropper
+            ref={cropperRef}
+            src={
+                imageUrl
+            }
+        />
+    );
+};
