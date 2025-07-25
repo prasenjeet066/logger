b620bd9a956926d299abaefb4ccf10f62c2b98cf
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-07-25 13:19:43 +0600

    update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,29 @@
 import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
+
 import type { NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request })
   
   const isAuthPage = request.nextUrl.pathname.startsWith('/auth/')
-
+  
   // Clone the request headers
   const requestHeaders = new Headers(request.headers)
   requestHeaders.set('x-auth-status', token ? 'authenticated' : 'unauthenticated')
-
+  
   // Handle auth pages (prevent authenticated users from accessing login/register)
   if (isAuthPage && token) {
     return NextResponse.redirect(new URL('/', request.url))
+  }
+  
+  const SPA = [
+    '/super-access'
+  ].some(path => request.nextUrl.pathname.startsWith(path))
+  if (SPA && !token || token?.superAccess ===null) {
+    const siUrl = new URL('/auth/sign-in', request.url)
+    siUrl.searchParams.set('callbackUrl', request.nextUrl.pathname)
+    return NextResponse.redirect(siUrl)
   }
   
   // Protected routes logic
@@ -23,20 +33,20 @@ export async function middleware(request: NextRequest) {
     '/settings',
     // Add more protected routes here
   ].some(path => request.nextUrl.pathname.startsWith(path))
-
+  
   if (isProtectedRoute && !token) {
     const signInUrl = new URL('/auth/sign-in', request.url)
     signInUrl.searchParams.set('callbackUrl', request.nextUrl.pathname)
     return NextResponse.redirect(signInUrl)
   }
-
+  
   // Set CORS headers
   const response = NextResponse.next({
     request: {
       headers: requestHeaders,
     },
   })
-
+  
   response.headers.set('Access-Control-Allow-Origin', '*') // Change '*' to specific domains if needed
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
@@ -45,7 +55,7 @@ export async function middleware(request: NextRequest) {
   if (request.method === 'OPTIONS') {
     return response // Return early for preflight requests
   }
-
+  
   return response
 }
 
