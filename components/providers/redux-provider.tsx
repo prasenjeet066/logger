ad4803350526ef0eebaea0ab37fc05f78d62b6d7
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-08-05 16:13:30 +0600

    update redux-provider.tsx

--- components/providers/redux-provider.tsx
+++ components/providers/redux-provider.tsx
@@ -2,7 +2,7 @@
 "use client"
 
 import { Provider } from 'react-redux'
-import { store } from '@/store/main.ts'
+import { store } from '@/store/main'
 
 interface ReduxProviderProps {
   children: React.ReactNode
