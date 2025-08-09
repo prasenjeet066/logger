a08386c3eaaece596379b818936a5c50bf95efee
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-08-09 11:23:43 +0600

    update reply-card.tsx

--- components/reply/reply-card.tsx
+++ components/reply/reply-card.tsx
@@ -253,7 +253,7 @@ export function ReplyCard({ post, onLike, onRepost }: PostCardProps) {
   return (
     <main>
       
-      <div className={"flex p-4 flex-col"}>
+      <div className={replies.length ? "flex p-4 flex-col" : "flex p-4 pb-2 flex-col"}>
         <div className ='flex flex-row items-center gap-2'>
            <Link
             href={`/profile/${post.author.username}`}
