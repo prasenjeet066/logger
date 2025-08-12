f239b913893c72f33dc10d5dbe799d2e6f9ffda3
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-08-12 13:33:20 +0600

    update icon.tsx

--- components/font-awesome/icon.tsx
+++ components/font-awesome/icon.tsx
@@ -42,7 +42,7 @@ export const Icon: React.FC<IconProps> = ({
   const classes = [
     prefix,
     name,
-    'inline-block',
+    'inline-block leading-none',
     size ? `fa-${size}` : "",
     spin ? "fa-spin" : "",
     pulse ? "fa-pulse" : "",
