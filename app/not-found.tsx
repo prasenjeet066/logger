2c9e43c3a45fa890ceee991a1bbef3fb39f36d7d
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-08-13 15:53:17 +0600

    update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,18 @@
 export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 px-4">
-      <h1 className='logo-font text-md'>logger</h1>
+      
       <div className="w-full max-w-md border-none shadow-none bg-gray-50">
-        <div className="text-center font-[transforma]">
+        <div className="text-center" style={{
+          fontFamily: 'transforma'
+        }}>
           <div className="text-2xl">404</div>
           <small>Your request isn't exist!</small>
         </div>
         <div>
           </div>
           </div>
+          <h1 className='logo-font text-md divide-y p-4'>logger</h1>
           </div>
   )
 }
