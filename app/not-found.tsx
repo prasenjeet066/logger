ec2ec862e582c98b5928d2146c83a74d37ef955f
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-08-13 15:49:16 +0600

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,11 @@
 export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 px-4">
-      <h1 className='logo-font text-sm'>logger</h1>
+      <h1 className='logo-font text-md'>logger</h1>
       <div className="w-full max-w-md border-none shadow-none bg-gray-50">
-        <div className="text-center">
-          <div className="text-2xl font-semibold">404/Unknown Page </div>
+        <div className="text-center font-[transforma]">
+          <div className="text-2xl">404</div>
+          <small>Your request isn't exist!</small>
         </div>
         <div>
           </div>
